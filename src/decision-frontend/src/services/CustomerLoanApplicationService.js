feaedc8afe0215fe984c721db52222ceaf998a66
Kaie Palmaru <kaiehe@yahoo.com>
2023-06-14 13:16:14 +0300

    removed front-end, will be deployed as a separate repo

--- src/decision-frontend/src/services/CustomerLoanApplicationService.js
+++
@@ -1,8 +0,0 @@
-import http from '../http-common'
-
-class CustomerLoanApplicationService {
-    apply(identityCode, amount, period) {
-    return http.get('/apply')}
-}
-
-export default new CustomerLoanApplicationService()
